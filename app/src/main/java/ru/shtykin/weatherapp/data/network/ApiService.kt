0b0000771271a35028710e537ada1324eddec79e
Shtykin <shtykin@mail.ru>
2023-07-17 22:20:03 +0800

    почистил

--- app/src/main/java/ru/shtykin/weatherapp/data/network/ApiService.kt
+++ app/src/main/java/ru/shtykin/weatherapp/data/network/ApiService.kt
@@ -1,6 +1,5 @@
 package ru.shtykin.weatherapp.data.network
 
-import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.*
 import ru.shtykin.weatherapp.data.model.CurrentWeatherDto
@@ -21,37 +20,5 @@ interface ApiService {
         @Query("days") days: String,
         @Query("lang") lang: String,
     ): Call<ForecastWeatherDto>
-
-//    suspend fun getCurrentWeather(@Path city: String): Response<CurrentWeatherDto>
-
-//    @GET("v1/current.json?key=a407be13669f42cc8e3150053231307&q=Moscow&aqi=no")
-//    suspend fun getCurrentWeather(): Response<CurrentWeatherDto>
-
-//    @GET("/")
-//    suspend fun checkServer(): String
-//
-//    @POST("/admin/login")
-//    suspend fun authAdmin(
-//        @Body authAdmin: AuthAdminDto
-//    ): Response<TokenDto>
-//
-//    @POST("/admin/users")
-//    suspend fun getAllUsers(): Response<List<UserDto>>
-//
-//    @POST("/admin/user")
-//    suspend fun getUser(
-//        @Body login: LoginDto
-//    ): Response<UserDto>
-//
-//    @POST("/admin/role")
-//    suspend fun setRole(
-//        @Body changeRoleDto: ChangeRoleDto
-//    ): Response<ChangeRoleDto>
-//
-//    @POST("/admin/owner")
-//    suspend fun addOwner(
-//        @Body addOwnerDto: AddOwnerDto
-//    ): Response<AddOwnerDto>
-
 
 }
